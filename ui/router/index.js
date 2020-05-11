d49d004d01c4671afb503cfbb8051883ba7f7bc5
stat-ki <per_ik@outlook.com>
2020-05-11 13:10:46 +0000

    Create route file

---
+++ ui/router/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const csurf = require('csurf');
+
+const router = express.Router();
+
+// Console to CLI.
+router.all('/*', (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Measures to CSRF.(https://www.shadan-kun.com/blog/measure/2640/)
+// This app doesn't hold cookie to prevent CSRF.
+router.use(csurf({
+  cookie: false,
+}));
+
+// Storing token setting.
+router.use((req, res, next) => {
+  const locals = res.locals;
+  locals.csrfToken = req.csrfToken();
+  return next();
+});
+
+router.get('/csrf-token', (req, res) => {
+  res.json({ token: res.locals.csrfToken });
+});
+
+router.use(express.static('public'));
+
+// Render login page.
+router.use('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.get('/logout', (req, res, next) => {
+  if (!req.session.user) {
+    // When use isn't logged in.
+    res.redirect('/login');
+    return;
+  }
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.redirect('/login');
+  });
+});
+
+const urlsRoutedLoginPage = /^\/(login(\/.*)?|logout)$/;
+
+const apisAccessibleWithoutLogin = /^\/api\/login$/;
+
+// Routing /login/*
+router.get(urlsRoutedLoginPage, (req, res) => {
+  res.render('login');
+});
+
+router.use((req, res, next) => {
+  if (apisAccessibleWithoutLogin.test(req.url)) {
+    next();
+    return;
+  }
+  const { session } = req;
+  //   Check authenticaton
+  const authenticated = session && session.authenticated;
+  if (authenticated) {
+    next();
+    return;
+  }
+  if (req.method !== 'GET' || /\/api\/.*/.test(req.url)) {
+    next({ status: 401 });
+    return;
+  }
+  res.redirect('/login');
+});
+
+router.use(express.static('public_authenticated'));
+
+router.use('/api', require('./api'));
+
+router.get('/*', (req, res) => {
+  res.header('Content-Type', 'text/html');
+  res.render('app');
+});
+
+module.exports = router;
